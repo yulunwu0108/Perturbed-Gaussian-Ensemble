f860f6ce08b38a7cde93a4cadebf6d510e7c93f4
Yulun Wu 吴宇伦 <wuyulun0108@outlook.com>
2026-05-02 23:37:33 -0500

    update submodules

--- radiative_gaussian/submodules/simple-knn/setup.py
+++ radiative_gaussian/submodules/simple-knn/setup.py
@@ -1,3 +1,14 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
@@ -13,7 +24,7 @@ setup(
         CUDAExtension(
             name="simple_knn._C",
             sources=[
-            "spatial.cu",
+            "spatial.cu", 
             "simple_knn.cu",
             "ext.cpp"],
             extra_compile_args={"nvcc": [], "cxx": cxx_compiler_flags})
